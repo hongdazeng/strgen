d0c3a3e8d947d9d0915222edab566a28e2564d48
Hongda Zeng <hongda.code@gmail.com>
2014-09-28 01:26:35 -0400

    Created StrgenTwo
    StrgenTwo will use overloaded construstors

---
+++ StrgenTwo.java
@@ -0,0 +1,17 @@
+/*strgen
+ * 
+ * This application creates a random string base on some random
+ * 
+ * Hongda Zeng (hongda.code@gmail.com)
+ * September 28, 2014
+ * */
+
+import java.util.*; //Getting Random, String, and Scanner
+
+public class StrgenTwo {
+    /* This method create a random string of a specified length using a pick line
+     * */
+    public static String randomString(int length) {
+        return null;
+    }
+}
--- strgen.java
+++ strgen.java
@@ -1,13 +1,17 @@
 /*strgen
  * 
- * This application creates a random string
+ * This application creates a random string base on some random
  * 
  * Hongda Zeng (hongda.code@gmail.com)
  * September 27, 2014
  * */
 
-import java.util.*;
+import java.util.*; //Getting Random, String, and Scanner
+
 public class Strgen {
+    /* This method create a random string of a specified length using a 
+     * 
+     * */
     public static String randomString(int length) {
         final String pickline = "0123456789abcdefghijklmnopqrestuvwxyz";
         // The method will choose random characters from the above line
@@ -20,10 +24,28 @@ public class Strgen {
         return builder.toString();
     }
     
+    public static String pickString(int length, String pick) {
+        // The method will choose random characters from a user chosen line
+        Random randGen = new Random();
+        // Create randomnese
+        StringBuilder builder = new StringBuilder(length);
+        for ( int i = 0; i < length; i++ ) {
+            builder.append ( pick.charAt(randGen.nextInt(pick.length())));
+        }
+        return builder.toString();
+    }
+    
     public static void main (String[] args) {
         Scanner reader = new Scanner (System.in);
         System.out.println ("Please enter how long of a string you need (a~z, 0~9: ");
         int strLen = reader.nextInt();
-        System.out.println ( randomString(strLen));
+        reader.nextLine();
+        System.out.println (">>> " + randomString(strLen));
+        System.out.println ("Pick String");
+        System.out.println ("Please enter the list of character you need: ");
+        String picklist = reader.nextLine();
+        System.out.println ("Please enter how long of a string you need (a~z, 0~9: ");
+        int strLen2 =reader.nextInt();
+        System.out.println (">>> " + pickString(strLen2, picklist));
     }
 }
